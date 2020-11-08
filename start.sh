410ded452155d258a68bbe8aaedbdb596ed9e846
Ross MacLean <ross@rossmac.co.uk>
2020-11-08 23:58:44 +0000

    Update port

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$ALD" -ne "TRUE"]; then
+#if [ "$ALD" -ne "TRUE"]; then
   echo "=> Starting apache"
   APACHE_CONFDIR="" source /etc/apache2/envvars
   rm -f "${APACHE_PID_FILE}"
@@ -14,5 +14,6 @@ if [ "$ALD" -ne "TRUE"]; then
   ls /run/teamcity
   cd /run/teamcity/bin
   exec /usr/local/bin/gosu cloudron:cloudron ./runAll.sh start
-  export ALD=TRUE
-fi
+#  export ALD=TRUE
+#fi
+exit 0
